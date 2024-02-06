52019a477deeceb2ba268953d90e920ded253918
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-02-06 09:37:29 -0700

    Added background image and gradient

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -7,12 +7,26 @@
       src="https://images.unsplash.com/photo-1574421233376-06f2ccf017f7?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       alt="">
   </div>
+  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
+    <div class="home-card p-5 card align-items-center shadow rounded elevation-3">
+      <h2>
+        Services
+      </h2>
+    </div>
+  </div>
+  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
+    <div class="home-card p-5 card align-items-center shadow rounded elevation-3">
+      <h2>
+        Reviews
+      </h2>
+    </div>
+  </div>
   <div class=" flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 align-items-center justify-content-center mt-3">
+    <div class="home-card p-5 mt-3">
       <h2>
         Hours
       </h2>
-      <table>
+      <table class="d-flex justify-content-center">
         <tr>
           <th>Sunday</th>
           <td>Closed</td>
@@ -45,20 +59,13 @@
     </div>
   </div>
   <div class=" flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 align-items-center mt-3 ">
+    <div class="home-card p-5 mt-3 ">
       <h2>
         Location
       </h2>
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2890.435316719675!2d-116.56382762434703!3d43.57664825737948!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54ae4d164cfb8781%3A0xaf91dccc44109896!2sFocus%20Massage!5e0!3m2!1sen!2sus!4v1707233805753!5m2!1sen!2sus"
         class="google-map" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    </div>
-  </div>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 card align-items-center shadow rounded elevation-3">
-      <h2>
-        Services
-      </h2>
     </div>
   </div>
   <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
@@ -110,6 +117,7 @@ export default {
 }
 
 .home-card {
+  text-align: center;
   width: clamp(350px, 70vw, 100%);
 }
 
@@ -121,7 +129,21 @@ export default {
   user-select: none;
 }
 
+table {
+  border-spacing: 20px;
+}
+
+th,
+td {
+  padding-left: 20px;
+  padding-right: 20px;
+}
+
 th {
+  text-align: left;
+}
+
+td {
   text-align: right;
 }
 </style>
