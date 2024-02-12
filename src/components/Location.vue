2f9c53bdc567299e5c0668b3235e22438e1efdd1
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-02-11 20:05:34 -0700

    migrated location content from HomePage to Location component

--- src/components/Location.vue
+++ src/components/Location.vue
@@ -1,5 +1,11 @@
 <template>
-  Location here
+  <h2>
+    Location
+  </h2>
+  <iframe
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2890.435316719675!2d-116.56382762434703!3d43.57664825737948!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54ae4d164cfb8781%3A0xaf91dccc44109896!2sFocus%20Massage!5e0!3m2!1sen!2sus!4v1707233805753!5m2!1sen!2sus"
+    class="google-map rounded mt-3" allowfullscreen="" loading="lazy"
+    referrerpolicy="no-referrer-when-downgrade"></iframe>
 </template>
 
 
