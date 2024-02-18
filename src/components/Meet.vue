a4819205834441867dc61ab096062636547ac505
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-02-17 20:36:35 -0700

    Style compenent and added placeholder content

--- src/components/Meet.vue
+++ src/components/Meet.vue
@@ -5,6 +5,21 @@
   <h3 class="mt-3">
     It's Dana!
   </h3>
+  <section class="row">
+    <div class="col-3 m-auto">
+      <img class="img-fluid"
+        src="https://images.unsplash.com/photo-1527236438218-d82077ae1f85?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="">
+    </div>
+    <div class="col-4 m-auto">
+      <h4>
+        Dana has been doing dana things since 20XX, <br>
+        Here is her favorite thing - Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus deleniti sunt
+        tempora architecto culpa ullam iste quis labore assumenda nesciunt nostrum atque sit eveniet fugiat, voluptatem
+        quia laborum in fugit.
+      </h4>
+    </div>
+  </section>
 </template>
 
 
