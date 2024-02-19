1a263b2705800152fb0c0d7f9596ffacac3d33fb
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-02-18 20:29:59 -0700

    Reformatting Meet component

--- src/components/Meet.vue
+++ src/components/Meet.vue
@@ -6,18 +6,20 @@
     It's Dana!
   </h3>
   <section class="row">
-    <div class="col-6 col-md-12">
+    <div class="col-12 col-md-6">
       <img class="img-fluid"
         src="https://images.unsplash.com/photo-1527236438218-d82077ae1f85?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt="">
     </div>
-    <div class="col-6 col-md-12">
+    <div class="col-12 col-md-6 mt-3 mt-md-0 d-flex flex-column justify-content-center">
       <h4>
-        Dana has been doing dana things since 20XX, <br>
+        Dana has been doing dana things since 20XX
+      </h4>
+      <h5>
         Here is her favorite thing - Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus deleniti sunt
         tempora architecto culpa ullam iste quis labore assumenda nesciunt nostrum atque sit eveniet fugiat, voluptatem
         quia laborum in fugit.
-      </h4>
+      </h5>
     </div>
   </section>
 </template>
@@ -35,5 +37,6 @@ export default {
 <style lang="scss" scoped>
 img {
   border-radius: 2px;
+  max-height: 50vh;
 }
 </style>
